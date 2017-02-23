0acf0091a1fb5e4a67e1b8286d9f8bc0d5dc4d3c
Michael Sun <msun2019@pingry.org>
2017-02-23 08:11:41 -0500

    Added Motorcycle stuff

--- Motorcycle.java
+++ Motorcycle.java
@@ -1,15 +1,25 @@
 public class Motorcycle extends Vehicle{
- private static int wheels = 2; 
- private int seats; 
+ protected static double fuelCapacity;
+ protected static int seats = 2; 
+ protected static double cargoCapacity;
+ protected static int wheels = 2;
+ protected static double baseWeight = 750;
  
  
  public Motorcycle(){
-  
+  fuelCapacity = 15; //Gallons  
+  cargoCapacity = 40;  //Pounds
+  money = 1000; //USD 
+  fuel = 0; //Gallons 
+  passengers = 2; //People 
+  cargo = 0; //Pounds
+  tires = 3; 
+  forwardProgress = 0; //Miles 
+  speed = 60; //Mph 
+  engine = new Engine(4); 
  }
  
- public Motorcycle(int windows, int seats){
- 
+ public boolean isStranded(){
+  return (tires < 2 || fuel <= 0 || money <= 0);
  }
- 
- 
 }
