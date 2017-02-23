caebc8d3ca48538c3ba71cad0bbb1ff51a076030
Michael Sun <msun2019@pingry.org>
2017-02-22 22:18:12 -0500

    Fixed a few bugs in RoadTrip and Motorcycle class

--- Motorcycle.java
+++ Motorcycle.java
@@ -1,30 +1,15 @@
 public class Motorcycle extends Vehicle{
- protected static double fuelCapacity;
- protected static int seats = 2; 
- protected static double cargoCapacity;
- protected static int wheels = 2;
- protected static double baseWeight = 750;
+ private static int wheels = 2; 
+ private int seats; 
  
  
  public Motorcycle(){
-  fuelCapacity = 15; //Gallons  
-  cargoCapacity = 40;  //Pounds
-  money = 1000; //USD 
-  fuel = 0; //Gallons 
-  passengers = 2; //People 
-  cargo = 0; //Pounds
-  tires = 3; 
-  forwardProgress = 0; //Miles 
-  speed = 60; //Mph 
+  
  }
  
-  public void fillGas(){
-  double gallons = 15 - fuel; 	
-  fuel = 15; 
-  money -= (gallons * 3); //each gallon costs 3 dollars 
+ public Motorcycle(int windows, int seats){
+ 
  }
  
- public boolean isStranded(){
-  return (tires < 2 || fuel <= 0 || money <= 0);
- }
+ 
 }
